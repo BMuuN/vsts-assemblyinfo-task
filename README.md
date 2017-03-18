9999fad0230fa1b19778b1ab7dc8ef331b58f9bb
Bleddyn Richards <bleddyn.richards@outlook.com>
2017-03-18 13:15:55 +0000

    and so it begins

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
-# vsts-assemblyinfo-task
+# Assembly Info
+Assembly Info is an extension for Team Foundation Server / Team Services that sets assembly information from a build.
+
+The extension will search the specified **Source folder** for all AssemblyInfo.\* source files and set the manifest data. This will result in assembly (*.dll) files containing common assembly information.
+
+The following fields can be set:
+
+| Attribute | Function |
+|-----------|-----------|
+| Title | Provides a title name for the assembly. |
+| Product | Provides the product information for the assembly. |
+| Description | Provides a detailed description of the product or modules that comprise the assembly. |
+| Company | Provides the company information for the assembly. |
+| Copyright | Provide the assembly or product copyright information. |
+| Trademark | Provides the assembly or product trademark information. |
+| Culture | Provides information on what language the assembly supports. |
+| Configuration | Provides the build configuration for the assembly, such as debug or release. |
+| Version Number | Provides a version number for the application. |
+| File Version Number | Provides a file version number for the application. |
+
+*If no value is specified for a particular field that field will be ignored and the default value in the AssemblyInfo source file will be used.*
+
+## How to use the build task
+### Configuration
+1. Create or edit a build definition.
+3. Click **Add build step...** and add the **Assembly Info** task from the Build category.
+4. Configure the task by providing values for the fields mentioned in the above table.  
+
+  ![Assembly Info task parameters](images/Task_Parameters.png)
+
+5. The result, an assembly with the manifest data applied.  
+
+  ![Assembly Info Set](images/Assembly_Manifest_Data.png)
+
+## Version Number
+A version number must be specified in the format `digit.digit.digit.digit`:  
+```
+1.0.0.0
+2016.12.31.1
+```
+To achieve the best result:
+1. From the **General** tab set the **Build number format** to:
+```
+$(Build.DefinitionName)_$(date:yyyy).$(date:MM).$(date:dd)$(rev:.r)
+```
+2. From the Assembly Info task set the **Version Number** to:
+```
+$(Build.BuildNumber)
+```
+This approach ensures:
+1. The version number is in the correct format.
+2. The version number is determined by the build and not the task.
+3. Enables us to associate assemblies (*.dll) to a specific build.
+
+## Copyright
+Set dates in the copyright field using the following examples:  
+```
+Copyright © $(date:yyyy) Example Ltd  
+Copyright © $(date:dd MM yyyy) Example Ltd  
+Copyright © $(date:M.d.yyyy) Example Ltd  
+Copyright © $(date:dd MMMM yyyy HH:mm tt) Example Ltd
+```
+
+## Release Notes
+> **v1.0.22**
+> - Added *File Version* task parameter.
+
+> **v1.0.21**
+> - Added date transforms for the *Copyright* task parameter.
+
+> **v1.0.14**
+> - Initial release
---
+++ task/ApplyAssemblyInfo.ps1
@@ -0,0 +1,188 @@
+[CmdletBinding()]
+param (
+
+    [Parameter(Mandatory)]
+    [String]$Path,
+
+    [Parameter(Mandatory=$false)]
+    [String]$Title,
+    
+    [Parameter(Mandatory=$false)]
+    [String]$Product,
+    
+    [Parameter(Mandatory=$false)]
+    [String]$Company,
+    
+    [Parameter(Mandatory=$false)]
+    [String]$Copyright,
+    
+    [Parameter(Mandatory=$false)]
+    [String]$Trademark,
+    
+    [Parameter(Mandatory=$false)]
+    [String]$Description,
+    
+    [Parameter(Mandatory=$false)]
+    [String]$Culture,
+    
+    [Parameter(Mandatory=$false)]
+    [String]$Configuration,
+
+    [Parameter(Mandatory=$false)]
+    [string]$VersionNumber,
+
+    [Parameter(Mandatory=$false)]
+    [string]$FileVersionNumber
+)
+
+Function ValidateVersionNumber($number)
+{
+    Write-Host "Validating: $number"
+
+    if(![string]::IsNullOrWhiteSpace($number)) {
+        $VersionData = [Regex]::Matches($number, $VersionRegex)
+        switch($VersionData.Count)
+        {
+        0        
+            { 
+                Write-Error "Could not find version number data in $number."
+                exit 1
+            }
+        1 
+            {
+                #Write-Host "Version Number is valid: $VersionData"
+                return $VersionData;
+            }
+        default 
+            { 
+                Write-Warning "Found more than instance of version data in $number." 
+                Write-Warning "Will assume first instance is version."
+                return $VersionData;
+            }
+        }
+
+        return [string]::Empty;
+    }
+}
+
+# Set a flag to force verbose as a default
+$VerbosePreference = 'Continue' # equiv to -verbose
+
+# Regular expression pattern to find the version
+$VersionRegex = "\d+\.\d+\.\d+\.\d+"
+
+# Regular expression pattern to find any string
+$WordRegex = ".*"
+
+# Date regular expression pattern
+$DateRegex = "(([\w.: +])*?)"
+
+# Make sure path to source code directory is available
+if (-not (Test-Path $Path))
+{
+    Write-Error "Source directory does not exist: $Path"
+    exit 1
+}
+
+$Version = ValidateVersionNumber $VersionNumber
+$FileVersion = ValidateVersionNumber $FileVersionNumber
+
+# write data to the output
+Write-Host "Source Directory: $Path"
+Write-Host "Version Parameter: $VersionNumber"
+Write-Host "Title: $Title"
+Write-Host "Version: $Version"
+Write-Host "File Version: $FileVersion"
+Write-Host "Product: $Product"
+Write-Host "Company: $Company"
+Write-Host "Copyright: $Copyright"
+Write-Host "Trademark: $Trademark"
+Write-Host "Description: $Description"
+Write-Host "Culture: $Culture"
+Write-Host "Configuration: $Configuration"
+
+# Get all AssemblyInfo files
+$files = Get-ChildItem $Path -Recurse -Include "*Properties*","My Project" | 
+    Where-Object { $_.PSIsContainer } | 
+    ForEach-Object { Get-ChildItem -Path $_.FullName -Recurse -Include "AssemblyInfo.*" }
+
+if($files)
+{
+    Write-Host "Applying assembly info to $($files.count) files..."
+
+    foreach ($file in $files) {
+        
+        # remove the read-only attribute, making the file writable
+        attrib $file -r
+
+        # get the file contents
+        $filecontent = Get-Content($file)
+
+        # set version
+        if(![string]::IsNullOrWhiteSpace($Version)) {
+            $filecontent -replace "AssemblyVersion\(`"$VersionRegex`"\)", "AssemblyVersion(`"$Version`")" | Out-File $file
+            $filecontent = Get-Content($file)
+        }
+
+        # set file version
+        if(![string]::IsNullOrWhiteSpace($FileVersion)) {
+            $filecontent -replace "AssemblyFileVersion\(`"$VersionRegex`"\)", "AssemblyFileVersion(`"$FileVersion`")" | Out-File $file
+            $filecontent = Get-Content($file)
+        }
+
+        # set title
+        if(![string]::IsNullOrWhiteSpace($Title)) {
+            $filecontent -replace "AssemblyTitle\(`"$WordRegex`"\)", "AssemblyTitle(`"$Title`")" | Out-File $file
+            $filecontent = Get-Content($file)
+        }
+
+        # set product
+        if(![string]::IsNullOrWhiteSpace($Product)) {
+            $filecontent -replace "AssemblyProduct\(`"$WordRegex`"\)", "AssemblyProduct(`"$Product`")" | Out-File $file
+            $filecontent = Get-Content($file)
+        }
+
+        # set company
+        if(![string]::IsNullOrWhiteSpace($Company)) {
+         $filecontent -replace "AssemblyCompany\(`"$WordRegex`"\)", "AssemblyCompany(`"$Company`")" | Out-File $file
+         $filecontent = Get-Content($file)
+        }
+
+        # set copyright
+        if(![string]::IsNullOrWhiteSpace($Copyright)) {
+            $Copyright = [Regex]::Replace($Copyright, "\$\(date:$DateRegex\)", { Get-Date -format $args.Groups[1].Value })
+            $filecontent -replace "AssemblyCopyright\(`"$WordRegex`"\)", "AssemblyCopyright(`"$Copyright`")" | Out-File $file
+            $filecontent = Get-Content($file)
+        }
+
+        # set trademark
+        if(![string]::IsNullOrWhiteSpace($Trademark)) {
+            $filecontent -replace "AssemblyTrademark\(`"$WordRegex`"\)", "AssemblyTrademark(`"$Trademark`")" | Out-File $file
+            $filecontent = Get-Content($file)
+        }
+
+        # set desription
+        if(![string]::IsNullOrWhiteSpace($Description)) {
+            $filecontent -replace "AssemblyDescription\(`"$WordRegex`"\)", "AssemblyDescription(`"$Description`")" | Out-File $file
+            $filecontent = Get-Content($file)
+        }
+
+        # set culture
+        if(![string]::IsNullOrWhiteSpace($Culture)) {
+            $filecontent -replace "AssemblyCulture\(`"$WordRegex`"\)", "AssemblyCulture(`"$Culture`")" | Out-File $file
+            $filecontent = Get-Content($file)
+        }
+
+        # set configuration
+        if(![string]::IsNullOrWhiteSpace($Configuration)) {
+            $filecontent -replace "AssemblyConfiguration\(`"$WordRegex`"\)", "AssemblyConfiguration(`"$Configuration`")" | Out-File $file
+            $filecontent = Get-Content($file)
+        }
+
+        Write-Host "$file - assembly info applied"
+    }
+}
+else
+{
+    Write-Warning "No Assembly Info files found."
+}
