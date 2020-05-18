b03cf089b4f41c93b22c7d61a4ce921b8375ef57
Bleddyn Richards <BMuuN@users.noreply.github.com>
2020-05-18 11:56:17 +0100

    Adding support for MSBuild.Sdk.Extras projects (#109)
    Update to support MSBuild.Sdk.Extras SDK
    
    Co-authored-by: Chris <crb04c@gmail.com>

--- src/netcore/index.ts
+++ src/netcore/index.ts
@@ -214,8 +214,8 @@ function setManifestData(model: models.NetCore, regEx: models.RegEx): void {
                 return;
             }
 
-            // Ensure the project is tartgeting .Net Core or .Net Standard
-            if (!result.Project.$.Sdk || result.Project.$.Sdk.indexOf('Microsoft.NET.Sdk') < 0) {
+            // Ensure the project is in the new format
+            if (!result.Project.$.Sdk || (result.Project.$.Sdk.indexOf('Microsoft.NET.Sdk') < 0 && result.Project.$.Sdk.indexOf('MSBuild.Sdk.Extras') < 0)) {
 
                 if (!model.ignoreNetFrameworkProjects) {
                     logger.warning(`Project is not targeting .Net Core or .Net Standard, moving to next file.`);
---
+++ tests/projects/MSBuildSdkExtras/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MSBuildSdkExtras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
